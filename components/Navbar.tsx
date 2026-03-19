0b49ed499636a89811c5ea6da1af3e32e23f4745
arvellacollective <arvellacollective@gmail.com>
2026-03-19 09:45:50 +0300

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,10 +28,11 @@ export default function Navbar() {
         transition-all duration-500 ease-out
         ${
           scrolled
-            ? "h-[64px] bg-white/95
+            ? "h-[64px] bg-white/95"
             : "h-[76px] bg-transparent"
         }
         `}
+		
 
       >
         <div className="max-w-[1440px] mx-auto h-full px-6 md:px-10 lg:px-16 xl:px-20">
@@ -72,15 +73,19 @@ export default function Navbar() {
               <Link
                 href="/shop"
                 className="
-                px-4 py-2
-                border border-black/20
-                rounded-[6px]
-                text-[12px] tracking-[0.28em]
-                text-neutral-900/90
-                hover:border-black hover:text-black
-                hover:bg-black/[0.03]
-                transition-all duration-300
-                "
+px-4 py-2
+border border-black/20
+rounded-[6px]
+text-[12px] tracking-[0.28em]
+text-neutral-900/90
+
+hover:border-black/40
+hover:text-neutral-900
+hover:bg-black/[0.04]
+
+
+transition-all duration-300 ease-out
+"
               >
                 SHOP
               </Link>
